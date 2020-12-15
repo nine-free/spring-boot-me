487435e83aa97d49a424aa1a1fe2a751cfb84458
bjzhangjifu <bjzhangjifu@corp.netease.com>
2020-12-15 15:58:42 +0800

    t

--- spring-boot-clickhouse/src/test/java/UserDaoTest.java
+++ spring-boot-clickhouse/src/test/java/UserDaoTest.java
@@ -5,6 +5,7 @@ import cn.soft1010.java.springboot.clickhouse.model.User;
 import com.alibaba.fastjson.JSONObject;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import org.assertj.core.util.DateUtil;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,6 +15,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.*;
 
 /**
  * Created by zhangjifu on 2020/12/13.
@@ -29,7 +31,7 @@ public class UserDaoTest {
     public void test() {
         User user1 = new User(1, "张三", "2020-10-01");
         User user2 = new User(2, "李四", "2020-10-01");
-        User user3 = new User(3, "王五", "2020-11-01");
+        User user3 = new User(3, "王五", "2020-10-01");
         List<User> users = new ArrayList<>();
         users.add(user1);
         users.add(user2);
@@ -54,19 +56,52 @@ public class UserDaoTest {
 
     @Test
     public void test2() {
-        PageHelper.startPage(1, 3);
-        List<User> users = userDao.queryUsersByDate("2020-01-01", "2020-03-01");
-//        Assert.assertEquals(1, users.size());
-        System.out.println(JSONObject.toJSON(users));
+        String[] dates = new String[]{"2020-01-01", "2020-02-01", "2020-03-01", "2020-04-01", "2020-05-01", "2020-06-01",
+                "2020-07-01", "2020-08-01", "2020-09-01", "2020-10-01", "2020-11-01", "2020-12-01",
+                "2019-01-01", "2019-02-01", "2019-03-01", "2019-04-01", "2019-05-01", "2019-06-01",
+                "2019-07-01", "2019-08-01", "2019-09-01", "2019-10-01", "2019-11-01", "2019-12-01"};
+        List<User> users = new ArrayList<>();
+        int num = 0;
+        for (String date : dates) {
+            users.add(new User(num++, "name", date));
+        }
+        long start = System.currentTimeMillis();
+        ExecutorService executorService = new ThreadPoolExecutor(20, 50, 0L, TimeUnit.MILLISECONDS,
+                new ArrayBlockingQueue<Runnable>(1000000));
+        List<Future> futures = new ArrayList<>();
+        for (int i = 0; i < 10000; i++) {
+            futures.add(executorService.submit(new Callable<Object>() {
+                @Override
+                public Object call() throws Exception {
+                    userDao.batchInsertUsers(users);
+                    return null;
+                }
+            }));
+        }
 
-        System.out.println("=======");
-        userDao.batchInsertUsers(users);
+        for (Future f : futures) {
+            try {
+                f.get();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(System.currentTimeMillis() - start);
     }
 
     @Test
     public void test3() {
-        userDao.deleteUser("2020-10-01");
-        userDao.deleteUser("2020-11-01");
+        String[] dates = new String[]{"2020-01-01", "2020-02-01", "2020-03-01", "2020-04-01", "2020-05-01", "2020-06-01",
+                "2020-07-01", "2020-08-01", "2020-09-01", "2020-10-01", "2020-12-01",
+                "2019-01-01", "2019-02-01", "2019-03-01", "2019-04-01", "2019-05-01", "2019-06-01",
+                "2019-07-01", "2019-08-01", "2019-09-01", "2019-10-01", "2019-11-01", "2019-12-01", "2020-11-01"};
+        for (String date : dates) {
+            userDao.deleteUser(date);
+        }
+        User user1 = new User(1, "张三", "2020-11-01");
+        userDao.saveUser(user1);
 
     }
 }
